6121b650ab0b3277ca0f1c52a378a4652b6584a4
Kodamas <superjason95@gmail.com>
2022-12-24 05:40:44 +0100

    fix: enable anti vpn

--- src/Controller/User/RegisterController.php
+++ src/Controller/User/RegisterController.php
@@ -76,7 +76,7 @@ class RegisterController extends DefaultController
 
             if (Utils::allowedFAI($host) === false) {
 
-                $isVpn = false; //Utils::isVPN($userIP, $host);
+                $isVpn = Utils::isVPN($userIP, $host);
                 
                 LogVpn::insert([
                     "ip" => $userIP,

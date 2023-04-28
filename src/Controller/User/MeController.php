275b1bda885223c312c9ed130f6c3edf9497c978
Kodamas <superjason95@gmail.com>
2023-04-28 13:20:28 +0200

    fix: remove premium check

--- src/Controller/User/MeController.php
+++ src/Controller/User/MeController.php
@@ -6,7 +6,6 @@ use App\Models\ForumThread;
 use App\Models\News;
 use App\Models\User;
 use App\Models\UserBadge;
-use App\Models\UserPremium;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Exception;
@@ -24,25 +23,10 @@ class MeController extends DefaultController
 
         $badgeMessage = $this->checkBadgeMiss($userId, $user->account_created);
 
-        $timevipexpire = 0;
-        $vip = UserPremium::where('user_id', $userId)->orderBy('timestamp_expire', 'DESC')->first();
-        if ($vip) {
-            if ($vip->timestamp_expire <= time()) {
-                UserPremium::where('user_id', $userId)->delete();
-
-                UserBadge::where('badge_id', 'WPREMIUM')->where('user_id', $userId)->delete();
-
-                User::where('id', $userId)->where('rank', '2')->update(['rank' => '1']);
-            }
-            $timevipexpire = $vip->timestamp_expire - time();
-            $timevipexpire = intval($timevipexpire / 60 / 60 / 24);
-        }
-
         $news = News::select('id', 'topstory_image', 'title', 'snippet', 'link_keyword')->where('timestamp', '<=', time())->orderBy('timestamp', 'DESC')->limit(5)->get();
         $lastforumthread = ForumThread::join('user', 'cms_forum_thread.author', '=', 'user.username')->select('cms_forum_thread.id', 'cms_forum_thread.title', 'cms_forum_thread.lastpost_author', 'cms_forum_thread.lastpost_date', 'user.look')->where('cms_forum_thread.type', 1)->orderBy('cms_forum_thread.lastpost_date', 'DESC')->limit(10)->get();
 
         $message = [
-            'vip_time' => $timevipexpire,
             'news' => $news,
             'forum' => $lastforumthread,
             'badgemessage' => $badgeMessage

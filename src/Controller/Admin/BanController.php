4b261e3b1c73b1b7326f379da57d835a6ef0442a
Ismael <88250803+ismaelpark@users.noreply.github.com>
2023-11-08 23:49:26 +0100

    BanController.PHP MAJ

--- src/Controller/Admin/BanController.php
+++ src/Controller/Admin/BanController.php
@@ -110,7 +110,7 @@ class BanController extends DefaultController
             throw new Exception('error', 400);
         }
 
-        if (($ban->added_by == "Kodamas" || $ban->added_by == "Jason") && $user->rank < 8) {
+        if (($ban->added_by == "Kodamas" || $ban->added_by == "Jason") && $user->rank < 11) {
             LogStaff::insert([
                 'pseudo' => $user->username,
                 'action' => 'Tentative de bannisement de ' . $name,

1876c00956e587546840ef40da5dd4d9d1313dee
Ethizyy <106849284+Ethizyy@users.noreply.github.com>
2024-01-05 16:39:11 +0100

    Get Profil by ID (fix)

--- src/Controller/User/ProfilController.php
+++ src/Controller/User/ProfilController.php
@@ -99,4 +99,75 @@ class ProfilController extends DefaultController
 
         return $this->jsonResponse($response, $message);
     }
+
+    public function getID(Request $request, Response $response, array $args): Response
+    {
+
+        $cacheData = $this->cache->get(10);
+        if(!empty($cacheData)) return $this->jsonResponse($response, $cacheData);
+
+        $profil = User::select('id', 'username', 'look', 'motto', 'online', 'limit_coins', 'vip_points', 'account_created', 'last_offline')->where('id', $args['id'])->first();
+
+        if (!$profil) {
+            throw new Exception('not-found', 404);
+        }
+
+        $stats = UserStats::where('id', $profil->id)->select('respect', 'achievement_score', 'online_time')->first();
+
+        if (!$stats) {
+            throw new Exception('not-found', 404);
+        }
+
+        $badgeCount = UserBadge::where('user_id', $profil->id)->count();
+
+        $totalPage = 0;
+
+        if ($badgeCount > 0) {
+            $totalPage = ceil($badgeCount / 40);
+        }
+
+        $groupe = Groups::where('owner_id', $profil->id)->orderBy('id')->get();
+
+        $countcoeur = MessengerFriendship::where('user_one_id', $profil->id)->where('relation', '1')->count();
+        $countami   = MessengerFriendship::where('user_one_id', $profil->id)->where('relation', '2')->count();
+        $countdead  = MessengerFriendship::where('user_one_id', $profil->id)->where('relation', '3')->count();
+
+        $randomcoeur = null;
+        $randomami = null;
+        $randomdead = null;
+
+        if ($countcoeur > 0) {
+            $randomcoeur = MessengerFriendship::where('user_one_id', $profil->id)->where('relation', '1')->leftJoin('user', 'messenger_friendship.user_two_id', '=', 'user.id')
+                ->inRandomOrder()->limit(1)->select('user.username')->first();
+        }
+
+        if ($countami > 0) {
+            $randomami = MessengerFriendship::where('user_one_id', $profil->id)->where('relation', '2')->leftJoin('user', 'messenger_friendship.user_two_id', '=', 'user.id')
+                ->inRandomOrder()->limit(1)->select('user.username')->first();
+        }
+
+        if ($countdead > 0) {
+            $randomdead = MessengerFriendship::where('user_one_id', $profil->id)->where('relation', '3')->leftJoin('user', 'messenger_friendship.user_two_id', '=', 'user.id')
+                ->inRandomOrder()->limit(1)->select('user.username')->first();
+        }
+
+        $message = [
+            'user' => $profil,
+            'stats' => $stats,
+            'groupe' => $groupe,
+            'totalPage' => $totalPage,
+            'badgecount' => $badgeCount,
+            'countcoeur' => $countcoeur,
+            'countami' => $countami,
+            'countdead' => $countdead,
+            'randomcoeur' => $randomcoeur,
+            'randomami' => $randomami,
+            'randomdead' => $randomdead,
+        ];
+
+        $this->cache->save($message);
+
+        return $this->jsonResponse($response, $message);
+
+    }
 }
